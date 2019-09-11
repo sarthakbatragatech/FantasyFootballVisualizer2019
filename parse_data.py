ca1ac0a45d582e0b9fb28e22281498c13aa52378
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-09-11 22:20:03 +0530

    Extracted all rows of players data

--- parse_data.py
+++ parse_data.py
@@ -15,10 +15,6 @@ html = open_html(cwd + '/' + position + '_urlrawhtml.txt')
     
 soup = BeautifulSoup(html, 'html.parser')
 
-# print(soup.prettify())
-
-data = []
-
 # Data is in a tabular format written in HTML
 # Find the table row which contains the column names
 columns = soup.find('tr', {'class': 'tableclmhdr'})
@@ -26,22 +22,38 @@ columns = soup.find('tr', {'class': 'tableclmhdr'})
 # The first player information is in the <tr> tag right after
 cur_player = columns.findNext('tr')
 
-info = cur_player.find_all('td', {'class': 'sort1'})
+finished = False
+# Initialize player_data to empty list
+player_data = []
 
-for i in range(len(info)):
-    # Special case to extract player name
-    if i == 0:
-        # Strip new line characters
-        text = info[i].get_text().replace('\n', '')
-        # String form is now '1. firstname lastname'
-        # Regex pattern to get everything after period
-        pattern = re.compile('\.(.*)')
-        match = pattern.search(text)
-        name = match.group(1).strip()
-        data.append(name)
-    else:
-        # For all other columns in the table
-        stat = info[i].get_text().strip()
-        data.append(stat)
-        
-print(data)
+while finished != True:
+    try:
+        info = cur_player.find_all('td', {'class': 'sort1'})
+
+        for i in range(len(info)):
+            # Special case to extract player name
+            if i == 0:
+                # Strip new line characters
+                text = info[i].get_text().replace('\n', '')
+                # String form is now '1. firstname lastname'
+                # Regex pattern to get everything after period
+                pattern = re.compile('\.(.*)')
+                match = pattern.search(text)
+                name = match.group(1).strip()
+                player_data.append(name)
+            else:
+                # For all other columns in the table
+                stat = info[i].get_text().strip()
+                player_data.append(stat)
+
+        # Last table row has no player data, hacky fix to not append it
+        if i == len(info) - 1:
+            print(player_data)
+            player_data = []
+
+        # Move onto the next player
+        cur_player = cur_player.findNext('tr')
+
+    except Exception as e:
+        print(e)
+        finished = True
